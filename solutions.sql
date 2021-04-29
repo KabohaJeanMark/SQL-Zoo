fe91012951fbdcc5fb4f67486dc3a3d99cf68d98
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-04-29 14:33:50 +0300

    Add Nobel Prize tutorial solutions

--- solutions.sql
+++ solutions.sql
@@ -123,5 +123,50 @@ SELECT name FROM world
 Brazil
 Colombia
 
+/* SELECT NOBEL PRIZE TUTORIAL*/
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
 
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
+
+SELECT winner
+FROM nobel
+WHERE subject = 'peace' AND yr >= 2000;
+
+SELECT *
+FROM nobel
+WHERE subject = 'Literature' AND  yr <= 1989 AND yr >= 1980;
+
+SELECT * FROM nobel
+WHERE winner IN 
+('Theodore Roosevelt',
+'Woodrow Wilson',
+'Jimmy Carter',
+'Barack Obama');
+
+SELECT winner 
+FROM nobel
+WHERE winner LIKE 'John%';
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE (subject = 'Physics' AND yr = 1980)
+OR (subject = 'Chemistry' AND yr = 1984);
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE (yr < 1910 AND subject = 'Medicine')
+OR (yr >= 2004 AND subject = 'Literature');
 
